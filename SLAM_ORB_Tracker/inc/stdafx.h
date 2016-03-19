800bf4ce5a07ce993b1ca1aa73d7186db5573d56
森郁 <senyu@mail.dlut.edu.cn>
2016-03-19 23:39:02 +0800

    hello world add opencv, boost link

---
+++ SLAM_ORB_Tracker/inc/stdafx.h
@@ -0,0 +1,30 @@
+//
+//  inc/stdafx.h
+//  SLAM_ORB_Tracker
+//
+//  Created by Sen Yu on 3/19/16.
+//  Copyright © 2016 Sen Yu. All rights reserved.
+//
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+
+// Boost Lib
+#include <boost/thread.hpp>
+
+
+
+// OpenCV Lib
+#include <opencv2/opencv.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/nonfree/features2d.hpp>
+#include <opencv2/legacy/legacy.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+
+
+// User-Defined Lib
+
---
+++ SLAM_ORB_Tracker/src/main.cpp
@@ -0,0 +1,16 @@
+//
+//  src/main.cpp
+//  SLAM_ORB_Tracker
+//
+//  Created by Sen Yu on 3/19/16.
+//  Copyright © 2016 Sen Yu. All rights reserved.
+//
+
+#include "stdafx.h"
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
